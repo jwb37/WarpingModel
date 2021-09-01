cf22209d566395bade0465c9328193344b6b33ac
Jack Bakes <jwbakes@fastmail.fm>
2021-09-01 10:56:00 +0100

    Remove obsolete models. Add acknowledgements for Pix2pix

--- models/__init__.py
+++ models/__init__.py
@@ -7,8 +7,6 @@ from .Pix2Pix.pix2pix_model import Pix2PixModel
 def get_model(visualizer=None):
     if Params.ModelName == 'Affine':
         return AffineModel(visualizer)
-    elif Params.ModelName == 'Translation':
-        return TranslationModel(visualizer)
     elif Params.ModelName == 'Mimic':
         return MimicModel(visualizer)
     elif Params.ModelName == 'Pix2Pix':
