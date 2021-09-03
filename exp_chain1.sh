88ff3b605799835edbaf8f70dc1e8405040cac67
Jack Bakes <jwbakes@fastmail.fm>
2021-09-03 11:12:29 +0100

    Fix it so that instructions in README complete as written (Make Mimic_Shoes depend on files in RTN_Shoes/Shoes not RTN_Shoes/Shoes(no_blur)

--- exp_chain1.sh
+++ exp_chain1.sh
@@ -3,17 +3,18 @@ python3 train.py RTN_Shoes
 if [ -d checkpoints/RTN_Shoes/Shoes ]; then rm -rf checkpoints/RTN_Shoes/Shoes; fi
 python3 test.py RTN_Shoes FlowGen
 python3 test.py RTN_Shoes WarpedGen
+
+python3 train.py Mimic_Shoes
+python3 test.py Mimic_Shoes WarpedGen
 mv checkpoints/RTN_Shoes/Shoes 'checkpoints/RTN_Shoes/Shoes(no_blur)'
+
+python3 train.py Pix2Pix_Shoes
+python3 test.py Pix2Pix_Shoes I2I
 
 if [ -d checkpoints/RTN_Shoes/Shoes ]; then rm -rf checkpoints/RTN_Shoes/Shoes; fi
 python3 test.py RTN_Shoes_Blur FlowGen
 python3 test.py RTN_Shoes_Blur WarpedGen
 mv checkpoints/RTN_Shoes/Shoes 'checkpoints/RTN_Shoes/Shoes(blur)'
-
-python3 train.py Mimic_Shoes
-python3 test.py Mimic_Shoes WarpedGen
-python3 train.py Pix2Pix_Shoes
-python3 test.py Pix2Pix_Shoes I2I
 
 python3 train.py Mimic_Shoes_Blur
 python3 test.py Mimic_Shoes_Blur WarpedGen
